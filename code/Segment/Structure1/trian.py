fb23f01b05316fa24bf4dbfcc33bc2b7e6f505c1
you-ming-hu <72851114+you-ming-hu@users.noreply.github.com>
2021-12-04 12:27:20 +0800

    daily commit

--- code/Segment/Structure1/trian.py
+++ code/Segment/Structure1/trian.py
@@ -6,8 +6,12 @@ import FLAGS
 FLAGS.CHECK()
 
 dataset = Data.Dataset(
-    label_folder = FLAGS.TRAIN.LABEL_FOLDER,
-    image_folder = FLAGS.TRAIN.IMAGE_FOLDER,
-    batch_size = FLAGS.TRAIN.BATCH_SIZE,
+    label_folder = FLAGS.DATA.TRAIN.LABEL_FOLDER,
+    image_folder = FLAGS.DATA.TRAIN.IMAGE_FOLDER,
+    batch_size = FLAGS.DATA.TRAIN.BATCH_SIZE,
     image_shape = INVARIANT.IMAGE_SHAPE,
     object_map_param = INVARIANT.OBJECT_MAP_PARAM)
+
+train_data,validation_data = dataset.split(
+    validation_ratio = FLAGS.DATA.TRAIN.VALIDATION_SPLIT_RATIO ,
+    random_state = FLAGS.DATA.TRAIN.VALIDATION_SPLIT_RANDOM_STATE)
